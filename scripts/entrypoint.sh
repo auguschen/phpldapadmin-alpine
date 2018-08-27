98ad7259e0402385e532b8f84c2aece6b37e0e43
Chen Augus <chenthao@163.com>
2018-08-28 06:39:57 +0800

    add initconf script and config example. change the entrypoint script, add init process

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,1 +1,7 @@
+#!/usr/bin/env sh
+
+if [ ! -f /.inited ]; then
+  sh /initconf.sh && touch /.inited
+fi
+
 php-fpm5 -D && nginx -g "daemon off;"
---
+++ scripts/initconf.sh
@@ -0,0 +1,4 @@
+// $servers->setValue('server','host','127.0.0.1');
+// $servers->setValue('server','port',389);
+// $servers->setValue('server','base',array(''));
+// $servers->setValue('login','auth_type','session');
